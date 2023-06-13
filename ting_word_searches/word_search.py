1fe7450f147b9431dfc8c1588f9e492d135d8267
Lucas Gravatá Portilho <lucasportilho18@gmail.com>
2023-06-13 16:35:30 -0300

    feat. requisito 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -17,4 +17,19 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = list()
+    for x in range(len(instance)):
+        ocorrencias = list()
+        for index, line in enumerate(instance.search(x)['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                ocorrencias.append({'linha': index + 1,
+                                    'conteudo': line})
+
+        if ocorrencias:
+            data.append({
+                'palavra': word,
+                'arquivo': instance.search(x)['nome_do_arquivo'],
+                'ocorrencias': ocorrencias,
+            })
+
+    return data
