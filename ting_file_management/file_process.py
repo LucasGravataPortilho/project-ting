070b05b37330619a803b7a8f4864b097b756dc78
Lucas Gravatá Portilho <lucasportilho18@gmail.com>
2023-06-13 14:59:19 -0300

    feat requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data,
+    }
+
+    for index in range(0, len(instance)):
+        searched = instance.search(index)
+        if searched == result:
+            return None
+
+    instance.enqueue(result)
+    print(result, file=sys.stdout)
 
 
 def remove(instance):
